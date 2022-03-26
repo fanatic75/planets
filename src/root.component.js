fb9b4d54295192f4bae57b429e0001f8ce7a864c
Prateek Banga <prateek.banga@techmojo.in>
2022-03-26 13:51:30 +0530

    added case to test cancel navigation

--- src/root.component.js
+++ src/root.component.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import PlanetPage from "./planets-page/planets-page.component.js";
-
+import { navigateToUrl } from "single-spa";
 export default function Root(props) {
+  useEffect(() => {
+    function cancelEvent(evnt) {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const params = Object.fromEntries(urlSearchParams.entries());
+      if (params.some) {
+        evnt.detail.cancelNavigation();
+        console.log(evnt.detail);
+      }
+    }
+    window.addEventListener("single-spa:before-routing-event", cancelEvent);
+    return () => {
+      window.removeEventListener(
+        "single-spa:before-routing-event",
+        cancelEvent
+      );
+    };
+  }, []);
   return (
-    <BrowserRouter>
-      <Route path="/planets" component={PlanetPage} />
-    </BrowserRouter>
+    <div onClick={() => navigateToUrl("/people?some=parm")}>
+      navigate to a different route
+    </div>
   );
 }
